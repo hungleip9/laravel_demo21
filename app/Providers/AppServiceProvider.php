9c600a3f178f8f427be0bc9918ae3f4216bf7b18
hungleip9 <66316466+hungleip9@users.noreply.github.com>
2020-12-13 20:31:30 +0700

    up heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,31 +32,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//        $danhmucs = Category::where('parent_id','-1')->get();
-//        $categories = Category::all();
-//        $products = Product::orderBy('updated_at','desc')->paginate(20);
-//        $prs = Product::orderBy('like','desc')->paginate(3);
-//        $imagess = Image::all();
-//        //header backend
-//        $ords = Order::all();
-//        $cmts = Comment::all();
-//        $uses = User::all();
-//        $ctgrs = Category::all();
-//        //end header backend
-//        View::share([
-//            'danhmucs' => $danhmucs,
-//            'products' => $products,
-//            'prs' => $prs,
-//            'imagess' => $imagess,
-//            'categories'=> $categories,
-//
-//            //header backend
-//            'ords' => $ords,
-//            'cmts' => $cmts,
-//            'uses' => $uses,
-//            'ctgrs' =>$ctgrs
-//            //end header backend
-//
-//        ]);
+        $danhmucs = Category::where('parent_id','-1')->get();
+        $categories = Category::all();
+        $products = Product::orderBy('updated_at','desc')->paginate(20);
+        $prs = Product::orderBy('like','desc')->paginate(3);
+        $imagess = Image::all();
+        //header backend
+        $ords = Order::all();
+        $cmts = Comment::all();
+        $uses = User::all();
+        $ctgrs = Category::all();
+        //end header backend
+        View::share([
+            'danhmucs' => $danhmucs,
+            'products' => $products,
+            'prs' => $prs,
+            'imagess' => $imagess,
+            'categories'=> $categories,
+
+            //header backend
+            'ords' => $ords,
+            'cmts' => $cmts,
+            'uses' => $uses,
+            'ctgrs' =>$ctgrs
+            //end header backend
+
+        ]);
     }
 }
